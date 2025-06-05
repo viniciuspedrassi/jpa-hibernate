f7e4c86277c9f9dbe4eb1f02668491718655b161
viniciuspedrassi <mviniciuscpedrassi@gmail.com>
2025-06-04 23:15:36 -0300

    Subindo arquivos do projeto para o GitHub

---
+++ aulajpamaven/src/main/java/aplicacao/Program.java
@@ -0,0 +1,31 @@
+package aplicacao;
+
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import dominio.Pessoa;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		Pessoa p = em.find(Pessoa.class, 2);
+		
+		em.getTransaction().begin();
+		em.remove(p);
+		em.getTransaction().commit();
+		
+		System.out.println("PRONTO");
+		
+		em.close();
+		emf.close();
+		
+	}
+
+}
